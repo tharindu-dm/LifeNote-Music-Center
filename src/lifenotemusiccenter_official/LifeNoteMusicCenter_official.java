d02a2f2c656868b6f0e66399954f9ec2c91b47bf
Tharindu Munasinghe <Tharindu Munasinghe@LAPTOP-6VMVM12E>
2020-10-28 11:49:28 +0530

    Commit 004 (LNMC_O) final 1

--- src/lifenotemusiccenter_official/LifeNoteMusicCenter_official.java
+++ src/lifenotemusiccenter_official/LifeNoteMusicCenter_official.java
@@ -14,6 +14,6 @@ public class LifeNoteMusicCenter_official {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Login().setVisible(true);
     }
 }
